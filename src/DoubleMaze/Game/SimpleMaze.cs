51b623e097f7a49703bfc529ee6c5b6018bb9b54
Mblkolo <rusakov-rv@yandex.ru>
2017-04-16 18:32:46 +0500

    Сделал загрузку и отображение лабиринта

--- src/DoubleMaze/Game/SimpleMaze.cs
+++ src/DoubleMaze/Game/SimpleMaze.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -6,14 +8,34 @@ using System.Threading.Tasks;
 
 namespace DoubleMaze.Game
 {
-    public class GameState
+    public interface IGameState
     {
+        [JsonConverter(typeof(StringEnumConverter))]
+        GameCommand command { get; }
+    }
+
+    public class PlayerPos : IGameState
+    {
+        public GameCommand command => GameCommand.PlayerState;
         public Pos pos { get; set; }
     }
+
+    public class MazeField : IGameState
+    {
+        public GameCommand command => GameCommand.MazeFeild;
+        public byte[,] field { get; set; }
+    }
+
 
     public class Pos {
         public float x;
         public float y;
+    }
+
+    public enum GameCommand
+    {
+        PlayerState,
+        MazeFeild
     }
 
     public enum InputCommand
@@ -27,14 +49,56 @@ namespace DoubleMaze.Game
 
     public class SimpleMaze
     {
-        private readonly Func<GameState, Task> callback;
+        private readonly Func<IGameState, Task> callback;
         private readonly Timer timer;
 
-        public SimpleMaze(Func<GameState, Task> callback)
+        public SimpleMaze(Func<IGameState, Task> callback)
         {
             this.callback = callback;
+
+            LoadMaze();
+            callback(new MazeField { field = mazeField });
+
             timer = new Timer(Update, new object(), 0, 100);
         }
+
+        private byte[,] mazeField;
+
+        private void LoadMaze()
+        {
+            var s = Resources.Resource.maze;
+            string[] lines = s.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int width = int.Parse(lines[0]);
+            int height = int.Parse(lines[1]);
+
+            bool[,] maze = new bool[height, width];
+            for (int y = 0; y < height; ++y)
+                for (int x = 0; x < width; ++x)
+                    maze[y, x] = lines[width * y + x + 2] == "0";
+
+            mazeField = new byte[height / 2, width / 2];
+            for (int y = 0; y < mazeField.GetLength(0); ++y)
+                for (int x = 0; x < mazeField.GetLength(1); ++x)
+                {
+                    int mazeX = 2 * x + 1;
+                    int mazeY = 2 * y + 1;
+
+                    if (maze[mazeY - 1, mazeX])
+                        mazeField[y, x] |= 1;
+
+                    if (maze[mazeY, mazeX + 1])
+                        mazeField[y, x] |= 2;
+
+                    if (maze[mazeY + 1, mazeX])
+                        mazeField[y, x] |= 4;
+
+                    if (maze[mazeY, mazeX - 1])
+                        mazeField[y, x] |= 8;
+                }
+
+        }
+
 
         private volatile InputCommand latestCommand;
 
@@ -55,7 +119,7 @@ namespace DoubleMaze.Game
 
 
 
-            callback(new GameState
+            callback(new PlayerPos
             {
                 pos = new Pos { x = PlayerPos.x, y = PlayerPos.y }
             });
