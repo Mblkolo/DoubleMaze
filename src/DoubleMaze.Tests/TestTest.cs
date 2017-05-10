2887ec5839d940f3905d308d4d3caf72c352c451
Mblkolo <rusakov-rv@yandex.ru>
2017-05-10 23:18:01 +0500

    Добавил тесты

---
+++ src/DoubleMaze.Tests/TestTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoubleMaze.Tests
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [Test]
+        public void Hello()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
