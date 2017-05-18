6d632081f2539acb6736fb71358d85b1879a972a
Mblkolo <rusakov-rv@yandex.ru>
2017-05-18 23:55:47 +0500

    Сделал тест очистки зоны победы

--- src/DoubleMaze.Tests/MazeGeneratorTest.cs
+++ src/DoubleMaze.Tests/MazeGeneratorTest.cs
@@ -12,9 +12,17 @@ namespace DoubleMaze.Tests
             {
                 return CreateWinZone(width, height);
             }
+
+            public Wall[,] CreateFieldWrapper(int width, int height)
+            {
+                return CreateField(width, height);
+            }
+
+            public void EraseWinZoneWrapper(Wall[,] field, RectZone winZone)
+            {
+                EraseWinZone(field, winZone);
+            }
         }
-
-
 
         [TestCase(2, 2, 0, 0, 2, 2)]
         [TestCase(3, 2, 0, 0, 3, 2)]
@@ -27,9 +35,41 @@ namespace DoubleMaze.Tests
             Assert.That(winZone, Is.EqualTo(new RectZone(rl, rt, rw, rh)));
         }
 
-        public void CheckWinZoneWalls()
+        [TestCase(2, 2, 0, 0, 2, 2)]
+        [TestCase(3, 2, 0, 0, 3, 2)]
+        [TestCase(2, 3, 0, 0, 2, 3)]
+        [TestCase(3, 3, 0, 0, 3, 3)]
+        [TestCase(4, 5, 1, 1, 2, 3)]
+        public void CheckWinZoneWalls(int width, int height, int left, int top, int rw, int rh)
         {
+            var mazeGenerator = new MazeGeneratorMock();
+            var field = mazeGenerator.CreateFieldWrapper(width, height);
 
+            mazeGenerator.EraseWinZoneWrapper(field, new RectZone(left, top, rw, rh));
+
+            var bottom = top + rh - 1;
+            var right = left + rw - 1;
+
+            Assert.That(field[top, left], Is.EqualTo(Wall.Top | Wall.Left));
+            Assert.That(field[top, right], Is.EqualTo(Wall.Top | Wall.Right));
+            Assert.That(field[bottom, right], Is.EqualTo(Wall.Bottom | Wall.Right));
+            Assert.That(field[bottom, left], Is.EqualTo(Wall.Bottom | Wall.Left));
+
+            for(int x = 1; x < rw - 1; ++x)
+            {
+                Assert.That(field[top, left + x], Is.EqualTo(Wall.Top));
+                Assert.That(field[bottom, left + x], Is.EqualTo(Wall.Bottom));
+            }
+
+            for (int y = 1; y < rh - 1; ++y)
+            {
+                Assert.That(field[top + y, left], Is.EqualTo(Wall.Left));
+                Assert.That(field[top + y, right], Is.EqualTo(Wall.Right));
+            }
+
+            for (int x = 1; x < rw - 1; ++x)
+                for (int y = 1; y < rh - 1; ++y)
+                    Assert.That(field[top + y, left+x], Is.EqualTo(Wall.None));
         }
     }
 }
--- src/DoubleMaze/Game/Maze/MazeGenerator.cs
+++ src/DoubleMaze/Game/Maze/MazeGenerator.cs
@@ -5,12 +5,23 @@ using System.Threading.Tasks;
 
 namespace DoubleMaze.Game
 {
+    [Flags]
+    public enum Wall : byte
+    {
+        None = 0,
+        Top = 1 << 0,
+        Right = 1 << 1,
+        Bottom = 1 << 2,
+        Left = 1 << 3,
+        Full = Top | Right | Bottom| Left
+    }
+
     public class MazeField
     {
-        public readonly byte[,] Field;
+        public readonly Wall[,] Field;
         public RectZone WinZone;
 
-        public MazeField(byte[,] field, RectZone winZone)
+        public MazeField(Wall[,] field, RectZone winZone)
         {
             Field = field;
             WinZone = winZone;
@@ -22,8 +33,19 @@ namespace DoubleMaze.Game
         public MazeField Generate(int width, int height)
         {
 
-            var field = new MazeField(new byte[height, width], CreateWinZone(width, height));
-            EraseWinZone(field);
+            var field = CreateField(width, height);
+            var winZone = CreateWinZone(width, height);
+            EraseWinZone(field, winZone);
+
+            return new MazeField(field, winZone);
+        }
+
+        protected Wall[,] CreateField(int width, int height)
+        {
+            var field = new Wall[height, width];
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; ++x)
+                    field[y, x] = Wall.Full;
 
             return field;
         }
@@ -35,9 +57,28 @@ namespace DoubleMaze.Game
             return new RectZone((width - rw) / 2, (height - rh) / 2, rw, rh);
         }
 
-        protected void EraseWinZone(MazeField field)
+        protected void EraseWinZone(Wall[,] field, RectZone winZone)
         {
+            for (int y = 0; y < winZone.Height; ++y)
+            {
+                for (int x = 0; x < winZone.Width; ++x)
+                {
+                    int realX = x + winZone.Left;
+                    int realY = y + winZone.Top;
 
+                    if (y != 0)
+                        field[realY, realX] &= (~Wall.Top);
+
+                    if (y != winZone.Height - 1)
+                        field[realY, realX] &= (~Wall.Bottom);
+
+                    if (x != 0)
+                        field[realY, realX] &= (~Wall.Left);
+
+                    if (x != winZone.Width-1)
+                        field[realY, realX] &= (~Wall.Right);
+                }
+            }
         }
     }
 }
