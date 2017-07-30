e31f700c0ff299346427fee1cca6fe9426b7f229
Mblkolo <rusakov-rv@yandex.ru>
2017-07-30 21:28:50 +0500

    Миграция. Таблица с юзерами

---
+++ src/DoubleMaze.Migrations/Migrations/Migration20170730_181700.cs
@@ -0,0 +1,45 @@
+﻿using SimpleMigrations;
+using System;
+
+namespace DoubleMaze.Migrations.Migrations
+{
+    [Migration(20170730181700)]
+    public class Migration20170730_181700 : Migration
+    {
+        protected override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Up()
+        {
+            Execute(@"CREATE SEQUENCE users_sequence");
+        }
+    }
+
+    [Migration(20170730181701)]
+    public class Migration20170730_181701 : Migration
+    {
+        protected override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Up()
+        {
+            Execute(@"
+                CREATE TABLE users
+                (
+                    id numeric NOT NULL DEFAULT nextval('users_sequence'),
+                    name text,
+                    is_activated boolean NOT NULL,
+                    player_type text,
+                    player_id uuid NOT NULL,
+                    payload jsonb NOT NULL,
+                    CONSTRAINT pk_users_id PRIMARY KEY (id),
+                    CONSTRAINT uk_users_player_type UNIQUE (player_id, player_type)
+                )
+            ");
+        }
+    }
+}
