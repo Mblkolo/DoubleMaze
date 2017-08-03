4e56f5234e98f974f92d513475ef806c68507144
rusakov <rusakov@byndyusoft.com>
2017-08-03 18:10:22 +0500

    Пытаюсь сделать базового актора

---
+++ src/DoubleMaze/Infrastructure/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace DoubleMaze.Infrastructure
+{
+    public abstract class Actor<T>
+    {
+        public readonly Pipe<T> Pipe = new Pipe<T>();
+
+        protected Actor(ILoggerFactory logerFactory)
+        {
+            logerFactory.CreateLogger(GetType());
+        }
+
+        public async Task Processor()
+        {
+            while (await Pipe.OutputAvailableAsync())
+            {
+                await ProcessAsync(await Pipe.ReceiveAsync());
+            }
+        }
+
+        public abstract Task ProcessAsync(T item);
+    }
+}
